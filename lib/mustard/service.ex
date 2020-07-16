3b58b41fd005f721d60e875208f8e63697d8a686
Prashanth Sadasivan <prashanth@fullstory.com>
2020-07-16 10:43:16 -0400

    wip two, node not rebalancing

--- lib/mustard/service.ex
+++ lib/mustard/service.ex
@@ -1,9 +1,10 @@
 defmodule Mustard.Service do
-  def ping() do
-    doc_idx = :riak_core_util.chash_key({"ping", :erlang.term_to_binary(:os.timestamp())})
-    pref_list = :riak_core_apl.get_primary_apl(doc_idx, 1, Mustard.Service)
-    [{index_node, _type}] = pref_list
+  def ping(v \\ 1) do
+    doc_idx = :riak_core_util.chash_key({"ping", "ping_#{v}"})
+    pref_list = :riak_core_apl.get_primary_apl(doc_idx, 2, Mustard.Service)
+    [{n1, _type}, {n2, _type}] = pref_list
     # riak core appends "_master" to Mustard.Vnode.
-    :riak_core_vnode_master.sync_spawn_command(index_node, :ping, Mustard.Vnode_master)
+    :riak_core_vnode_master.sync_spawn_command(n1, {:ping, v}, Mustard.Vnode_master)
+    :riak_core_vnode_master.sync_spawn_command(n2, {:ping, v}, Mustard.Vnode_master)
   end
 end
--- lib/mustard/vnode.ex
+++ lib/mustard/vnode.ex
@@ -10,16 +10,12 @@ defmodule Mustard.Vnode do
     {:ok, %{partition: partition}}
   end
 
-  def handle_command(:ping, _sender, %{partition: partition} = state) do
+  def handle_command({:ping, v}, _sender, state = %{partition: partition}) do
     Logger.info("got a ping request!")
-    {:reply, {:pong, partition}, state}
+    {:reply, {:pong, v + 1, node(), partition}, state}
   end
 
-  def handle_handoff_command(_fold_req, _sender, state) do
-    {:noreply, state}
-  end
-
-  def handoff_starting(_target_node, state) do
+  def handoff_starting(_dest, state) do
     {true, state}
   end
 
@@ -27,35 +23,47 @@ defmodule Mustard.Vnode do
     {:ok, state}
   end
 
-  def handoff_finished(_target_node, state) do
+  def handoff_finished(_dest, state) do
     {:ok, state}
   end
 
-  def handle_handoff_data(data, state) do
-    {:reply, :ok, state}
-  end
-
-  def encode_handoff_item(object_name, object_value) do
-    ""
+  def handle_handoff_command(_fold_req, _sender, state) do
+    {:noreply, state}
   end
 
   def is_empty(state) do
     {true, state}
   end
 
+  def terminate(_reason, _state) do
+    :ok
+  end
+
   def delete(state) do
     {:ok, state}
   end
 
-  def handle_coverage(req, key_spaces, sender, state) do
+  def handle_handoff_data(_bin_data, state) do
+    {:reply, :ok, state}
+  end
+
+  def encode_handoff_item(_k, _v) do
+  end
+
+  def handle_coverage(_req, _key_spaces, _sender, state) do
     {:stop, :not_implemented, state}
   end
 
-  def handle_exit(pid, reason, state) do
+  def handle_exit(_pid, _reason, state) do
     {:noreply, state}
   end
 
-  def terminate(reason, state) do
+  def handle_overload_command(_, _, _) do
     :ok
   end
+
+  def handle_overload_info(_, _idx) do
+    :ok
+  end
+
 end
